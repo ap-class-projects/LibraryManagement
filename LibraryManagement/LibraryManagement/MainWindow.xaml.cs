a605356a44f3fb4826336273a96e306defdb7274
Hamed Feizabadi <hamedfz19@gmail.com>
2021-06-06 09:34:34 +0430

    start!

---
+++ LibraryManagement/LibraryManagement/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void signInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(emailBox.Text == "a")
+            {
+                //show another window
+                //MessageBox.Show("Logged in!");
+                User user = new User();
+                user.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Wrong!");
+            }
+        }
+    }
+}
