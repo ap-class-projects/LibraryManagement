e6e5ef4d988193930a6040fad7cfdb22298e67ab
Hamed Feizabadi <hamedfz19@gmail.com>
2021-06-08 16:36:02 +0430

    login added

--- LibraryManagement/LibraryManagement/MainWindow.xaml.cs
+++ LibraryManagement/LibraryManagement/MainWindow.xaml.cs
@@ -32,33 +32,37 @@ namespace LibraryManagement
 
         private void signInBtn_Click(object sender, RoutedEventArgs e)
         {
-            SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\MyProjects\LibraryManagement\LibraryManagement\LibraryManagement\database\db.mdf;Integrated Security=True;Connect Timeout=30");
-            sqlConnection.Open();
-            string command = "select * from UsersInfo";
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command, sqlConnection);
-            DataTable dataTable = new DataTable();
-            sqlDataAdapter.Fill(dataTable);
-            for (int i = 0; i < dataTable.Rows.Count; i++)
+            if (emailUserNameBox.Text == "" || passwordBox.Password == "")
             {
-                MessageBox.Show($"{dataTable.Rows[i][0]}");
+                MessageBox.Show("please fill both fields!");          
             }
-            //SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
-            //sqlCommand.BeginExecuteNonQuery();
-            sqlConnection.Close();
+            else
+            {
+                bool loggedIn = false;
+                SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\MyProjects\LibraryManagement\LibraryManagement\LibraryManagement\database\db.mdf;Integrated Security=True;Connect Timeout=30");
+                sqlConnection.Open();
+                string command = "select * from UsersInfo";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command, sqlConnection);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    if ((string)dataTable.Rows[i][0] == emailUserNameBox.Text && (string)dataTable.Rows[i][3] == passwordBox.Password)
+                    {
+                        loggedIn = true;
+                    }
+                }
+                sqlConnection.Close();
 
-
-            //if (emailUserNameBox.Text == "a")
-            //{
-            //    //show another window
-            //    MessageBox.Show("Logged in!");
-            //    User user = new User();
-            //    user.Show();
-            //    this.Close();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Wrong!");
-            //}
+                if (loggedIn)
+                {
+                    MessageBox.Show("logged in!");
+                }
+                else
+                {
+                    MessageBox.Show("wrong!");
+                }
+            }
         }
     }
 }
