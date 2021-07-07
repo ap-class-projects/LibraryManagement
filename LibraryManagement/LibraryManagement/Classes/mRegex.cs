74af55f9638079eef2650b84cb14bcac17c6ba12
Hamed Feizabadi <hamedfz19@gmail.com>
2021-07-07 11:57:56 +0430

    admin UI update

--- LibraryManagement/LibraryManagement/Classes/mRegex.cs
+++ LibraryManagement/LibraryManagement/Classes/mRegex.cs
@@ -2,6 +2,18 @@
 {
     static class mRegex
     {
-
+        //public static bool emailIsValid(string email)
+        //{
+        //    string pattern = @"^[a-zA-Z][a-zA-Z0-9]*@[a-zA-Z][a-zA-Z0-9]*\.[a-zA-Z]+";
+        //    Regex regex = new Regex(pattern);
+        //    if (regex.IsMatch(email))
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
     }
 }
