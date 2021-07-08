a3e0c33a39cbd24f16be71f28edd20624e48a2d9
Hamed Feizabadi <hamedfz19@gmail.com>
2021-07-08 09:20:56 +0430

    regex class completed

--- LibraryManagement/LibraryManagement/Classes/mRegex.cs
+++ LibraryManagement/LibraryManagement/Classes/mRegex.cs
@@ -1,19 +1,94 @@
-﻿namespace LibraryManagement.Classes
+﻿using System.Text.RegularExpressions;
+
+namespace LibraryManagement.Classes
 {
     static class mRegex
     {
-        //public static bool emailIsValid(string email)
-        //{
-        //    string pattern = @"^[a-zA-Z][a-zA-Z0-9]*@[a-zA-Z][a-zA-Z0-9]*\.[a-zA-Z]+";
-        //    Regex regex = new Regex(pattern);
-        //    if (regex.IsMatch(email))
-        //    {
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
+        public static bool userNameIsValid(string userName)
+        {
+            string pattern = @"^[a-zA-Z][a-zA-Z\d]{2,31}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(userName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool nameIsValid(string name)
+        {
+            string pattern = @"^[a-zA-Z][a-zA-Z]{1,30}[a-zA-Z]$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool emailIsValid(string email)
+        {
+            string pattern = @"^[a-zA-Z][a-zA-Z\d_-]{0,31}@[a-zA-Z][a-zA-Z\d]{0,7}\.[a-zA-Z]{0,2}[a-zA-Z]$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(email))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool phoneNumberIsValid(string phoneNumber)
+        {
+            string pattern = @"^09\d{8}\d$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(phoneNumber))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool passwordIsValid(string password)
+        {
+            //(?=.*[a-z]) The string must contain at least 1 lowercase alphabetical character
+            //(?=.*[A-Z]) The string must contain at least 1 uppercase alphabetical character
+            //(?=.*[0-9]) The string must contain at least 1 numeric character
+            string pattern = @"^(?=.*[A-Z])[a-zA-Z\d]{8,32}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool cvvIsValid(string cvv)
+        {
+            string pattern = @"^\d{3,4}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(cvv))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- LibraryManagement/LibraryManagement/Pages/SignUpPage.xaml.cs
+++ LibraryManagement/LibraryManagement/Pages/SignUpPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using LibraryManagement.Classes;
 
 namespace LibraryManagement.Pages
 {
@@ -16,6 +19,57 @@ namespace LibraryManagement.Pages
         private void signInButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             changeToLoginPage();
+        }
+
+        private bool fieldIsEmpty()
+        {
+            if(userNameBox.Text == "" ||
+               firstNameBox.Text == "" ||
+               lastNameBox.Text == "" ||
+               emailBox.Text == "" ||
+               phoneNumberBox.Text == "" ||
+               passwordBox.Password == "" ||
+               confirmPasswordBox.Password == "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool userNameExists()
+        {
+            DataTable dataTable = PeopleTable.read();
+            for(int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                if(userNameBox.Text == dataTable.Rows[i][PeopleTable.indexUserName].ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void signUpButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(fieldIsEmpty())
+            {
+                MessageBox.Show("Please fill all of the given fields!");
+            }
+            else
+            {
+                if(userNameExists())
+                {
+                    MessageBox.Show("This username already exists!");
+                }
+                else
+                {
+                    //check regex
+                    //sign up
+                }
+            }
         }
     }
 }
