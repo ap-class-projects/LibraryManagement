81b5e34b6b3246ac9bafae8c4556d570550c3624
mr-zare <74895748+mr-zare@users.noreply.github.com>
2021-07-08 23:11:07 +0430

    Update Employee.cs

--- LibraryManagement/LibraryManagement/Classes/Employee.cs
+++ LibraryManagement/LibraryManagement/Classes/Employee.cs
@@ -1,12 +1,48 @@
-﻿namespace LibraryManagement.Classes
+namespace LibraryManagement.Classes
 {
     class Employee : Person
     {
         public Employee(string userName, string firstName, string lastName,
-            Role role, string phoneNumber, string email, string password)
-        : base(userName, firstName, lastName, role, phoneNumber, email, password)
+            Role role, string phoneNumber, string email, string password,double moneyBag)
+        : base(userName, firstName, lastName, role, phoneNumber, email, password,moneyBag)
+        {
+        }
+        public void seebooks()
         {
 
         }
+        public void seeborrowbook()
+        {
+
+        }
+        public void seeavailablebook()
+        {
+
+        }
+        public void seeusers()
+        {
+
+        }
+        public void seedelaygivebackman()
+        {
+
+        }
+        public void seedelaypayman()
+        {
+
+        }
+        public void seeperson(Person a)
+        {
+
+        }
+        public void seecash()
+        {
+
+        }
+        public void changeinfo()
+        { 
+            
+        }
+        
     }
 }
