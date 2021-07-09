6978b1c53a6ccca8e80fe57397f5b7339a00aee0
Hamed Feizabadi <hamedfz19@gmail.com>
2021-07-09 13:22:43 +0430

    user ui

--- LibraryManagement/LibraryManagement/Pages/UserPanelPage.xaml.cs
+++ LibraryManagement/LibraryManagement/Pages/UserPanelPage.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using LibraryManagement.Classes;
+using Microsoft.Win32;
+using System;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace LibraryManagement.Pages
 {
@@ -9,6 +13,7 @@ namespace LibraryManagement.Pages
     public partial class UserPanelPage : Page
     {
         public event PageChangerNoArg changeToLoginPage;
+        public ObservableCollection<string> borrowedBooks { get; set; }
         User user;
 
         public UserPanelPage(PageChangerNoArg changeToLoginPage, User user)
@@ -16,6 +21,39 @@ namespace LibraryManagement.Pages
             InitializeComponent();
             this.changeToLoginPage = changeToLoginPage;
             this.user = user;
+
+            borrowedBooks = new ObservableCollection<string>();
+            borrowedBooks.Add("1");
+            borrowedBooks.Add("2");
+            this.DataContext = this;
+        }
+
+        private void uploadButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                image.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void logOutbutton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            changeToLoginPage();
+        }
+
+        private void checkBox1_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            checkBox2.IsChecked = false;
+        }
+
+        private void checkBox2_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            checkBox1.IsChecked = false;
         }
     }
 }
