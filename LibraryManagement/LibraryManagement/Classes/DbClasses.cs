260dc30a9b5bfba9e6047986c8119d5b5ae45a8a
mr-zare <74895748+mr-zare@users.noreply.github.com>
2021-07-09 19:09:06 +0430

    Update DbClasses.cs

--- LibraryManagement/LibraryManagement/Classes/DbClasses.cs
+++ LibraryManagement/LibraryManagement/Classes/DbClasses.cs
@@ -19,7 +19,7 @@ namespace LibraryManagement.Classes
                 Thread.Sleep(4000);
                 sqlConnection.Close();
             }
-            
+
             Thread thread = new Thread(close);
             thread.Start();
         }
@@ -63,7 +63,7 @@ namespace LibraryManagement.Classes
         {
             SqlConnection sqlConnection = new SqlConnection(connectionString);
             sqlConnection.Open();
-            string command = "insert into People values ('"+ person.userName +"', '"+ person.firstName +"', '"+ person.lastName +"', '"+ person.role.ToString() +"', '"+ person.phoneNumber +"', '"+ person.email +"', '"+ person.password +"','"+person.moneyBag+"')";
+            string command = "insert into People values ('" + person.userName + "', '" + person.firstName + "', '" + person.lastName + "', '" + person.role.ToString() + "', '" + person.phoneNumber + "', '" + person.email + "', '" + person.password + "','" + person.moneyBag + "', '" + null + "')";
             SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
             sqlCommand.BeginExecuteNonQuery();
             sqlConnection.delayedClose();
@@ -77,7 +77,7 @@ namespace LibraryManagement.Classes
         {
             SqlConnection sqlConnection = new SqlConnection(connectionString);
             sqlConnection.Open();
-            string command = "delete from People where userName = '"+ userName +"'";
+            string command = "delete from People where userName = '" + userName + "'";
             SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
             sqlCommand.BeginExecuteNonQuery();
             sqlConnection.delayedClose();
@@ -91,7 +91,7 @@ namespace LibraryManagement.Classes
         {
             SqlConnection sqlConnection = new SqlConnection(connectionString);
             sqlConnection.Open();
-            string command = "update People SET userName = '"+ person.userName +"', firstName = '"+ person.firstName +"', lastName = '"+ person.lastName +"', role = '"+ person.role.ToString() +"', phoneNumber = '"+ person.phoneNumber +"', email = '"+ person.email +"', password = '"+ person.password +"',moneyBag = '"+person.moneyBag+"' where name ='"+ oldUserName +"' ";
+            string command = "update People SET userName = '" + person.userName + "', firstName = '" + person.firstName + "', lastName = '" + person.lastName + "', role = '" + person.role.ToString() + "', phoneNumber = '" + person.phoneNumber + "', email = '" + person.email + "', password = '" + person.password + "',moneyBag = '" + person.moneyBag + "' where name ='" + oldUserName + "' ";
             SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
             sqlCommand.BeginExecuteNonQuery();
             sqlConnection.delayedClose();
@@ -110,21 +110,128 @@ namespace LibraryManagement.Classes
             FileInfo DbFile = new FileInfo(path);
             connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + DbFile.FullName + ";Integrated Security=True;Connect Timeout=30";
         }
+        public const int indexName = 0;
+        public const int indexWriter = 1;
+        public const int indexGenre = 2;
+        public const int indexPrintingNumber = 3;
+        public const int indexcount = 4;
+        public static DataTable read()
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string command = "select * from Books";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command, sqlConnection);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.delayedClose();
+            return dataTable;
+        }
 
-        //public static DataTable read()
-        //{
-        //}
+        public static void write(Book book)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string command = "insert into Books values ('" + book.name + "', '" + book.writer + "', '" + book.genre + "', '" + book.printingNumber + "', '" + book.count + "', '" + null + "')";
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            sqlCommand.BeginExecuteNonQuery();
+            sqlConnection.delayedClose();
+        }
 
-        //public static void write(Book book)
-        //{ 
-        //}
+        public static void delete(string printingNumber)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string command = "delete from Books where printingNumber = '" + printingNumber + "'";
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            sqlCommand.BeginExecuteNonQuery();
+            sqlConnection.delayedClose();
+        }
 
-        //public static void delete(string bookId)
-        //{
-        //}
+        public static void update(string oldBookId, Book book)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string command = "update People SET name = '" + book.name + "', writer = '" + book.writer + "', genre = '" + book.genre + "', printingNumber = '" + book.printingNumber + "', count = '" + book.count + "' ";
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            sqlCommand.BeginExecuteNonQuery();
+            sqlConnection.delayedClose();
+        }
+    }
+    static class UsersInfos
+    {
+        static string connectionstring;
+        static UsersInfos()
+        {
+            string path = Environment.CurrentDirectory + @"\..\..\Database\db.mdf";
+            FileInfo DbFile = new FileInfo(path);
+            connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + DbFile.FullName + ";Integrated Security=True;Connect Timeout=30";
+        }
 
-        //public static void update(string oldBookId, Book book)
-        //{
-        //}
+        public const int indexUserName = 0;
+        public const int indexBook1 = 1;
+        public const int indexExpireDate1 = 2;
+        public const int indexBook2 = 3;
+        public const int indexExpireDate2 = 4;
+        public const int indexBook3 = 5;
+        public const int indexExpireDate3 = 6;
+        public const int indexBook4 = 7;
+        public const int indexExpireDate4 = 8;
+        public const int indexBook5 = 9;
+        public const int indexExpireDate5 = 10;
+        public const int indexRegisterDate = 11;
+        public const int indexRenewalDate = 12;
+        public const int indexExpireDateTime = 13;
+        public static DataTable read()
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionstring);
+            sqlConnection.Open();
+            string command = "select * from UsersInfos";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command, sqlConnection);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.delayedClose();
+            return dataTable;
+        }
+
+        /*
+        public static void write(Person person)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionstring);
+            sqlConnection.Open();
+            string command = "insert into People values ('" + person.userName + "', '" + person.firstName + "', '" + person.lastName + "', '" + person.role.ToString() + "', '" + person.phoneNumber + "', '" + person.email + "', '" + person.password + "','" + person.moneyBag + "')";
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            sqlCommand.BeginExecuteNonQuery();
+            sqlConnection.delayedClose();
+        }
+        */
+
+        /// <summary>
+        /// delete from People table by userName
+        /// </summary>
+        /// <param name="userName"></param>
+        public static void delete(string userName)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionstring);
+            sqlConnection.Open();
+            string command = "delete from UsersInfos where userName = '" + userName + "'";
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            sqlCommand.BeginExecuteNonQuery();
+            sqlConnection.delayedClose();
+        }
+
+        /// <summary>
+        /// !before update check person exists!
+        /// </summary>
+        /// <param name="person"></param>
+        /*    public static void update(string oldUserName, Person person)
+            {
+                SqlConnection sqlConnection = new SqlConnection(connectionstring);
+                sqlConnection.Open();
+                string command = "update People SET userName = '" + person.userName + "', firstName = '" + person.firstName + "', lastName = '" + person.lastName + "', role = '" + person.role.ToString() + "', phoneNumber = '" + person.phoneNumber + "', email = '" + person.email + "', password = '" + person.password + "',moneyBag = '" + person.moneyBag + "' where name ='" + oldUserName + "' ";
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlCommand.BeginExecuteNonQuery();
+                sqlConnection.delayedClose();
+            }
+        */
     }
 }
