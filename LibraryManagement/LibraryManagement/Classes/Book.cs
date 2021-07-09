941c529387c54c0662f86ef5990bff92ae73eed8
mr-zare <74895748+mr-zare@users.noreply.github.com>
2021-07-09 19:08:24 +0430

    Update Book.cs

--- LibraryManagement/LibraryManagement/Classes/Book.cs
+++ LibraryManagement/LibraryManagement/Classes/Book.cs
@@ -1,6 +1,19 @@
-﻿namespace LibraryManagement.Classes
+namespace LibraryManagement.Classes
 {
-    class Book
+    public class Book
     {
+        public string name { get; }
+        public string writer { get; }
+        public string genre { get; }
+        public int printingNumber  { get; }
+        public int count { get; }
+        public Book(string name, string writer,string genre,int printingNumber,int count)
+        {
+            this.name = name;
+            this.writer = writer;
+            this.genre = genre;
+            this.printingNumber = printingNumber;
+            this.count = count;
+        }
     }
 }
